5af7cc24721a389083124d367088c9912d4c4a88
Maura Gallagher <maura.gallagher@simplybusiness.com>
2020-04-09 19:21:12 +0000

    Thu Morning session

--- models/customers.sql
+++ models/customers.sql
@@ -1,16 +1,17 @@
-{{
-  config(
-    materialized='table'
-  )
-}}
-
-with customer_orders as (
+with customers as (
+    select * from {{ ref('stg_customers')}}
+),
+orders as (
+    select * from {{ ref('orders')}}
+),
+customer_orders as (
     select
         customer_id,
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-    from {{ ref('stg_orders') }}
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
+    from orders
     group by 1
 ),
 final as (
@@ -20,8 +21,9 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
-        coalesce(customer_orders.number_of_orders, 0) as number_of_orders
-    from {{ ref('stg_customers') }} customers
+        coalesce(customer_orders.number_of_orders, 0) as number_of_orders,
+        coalesce(customer_orders.lifetime_value, 0) as lifetime_value
+    from customers
     left join customer_orders using (customer_id)
 )
 select * from final
